a7e1264c9590c196ee024cf68d85fe906b94a733
Guilherme Anselmo de Sa <94063314+GuilhermeAnselmodeSa@users.noreply.github.com>
2024-08-18 22:52:22 -0300

    Add files via upload

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './header.css'
+import { MdLogin } from "react-icons/md";
+import { HiMiniPencilSquare } from "react-icons/hi2";
 import logo from '../../img/logo.png'
 
 function Header() {
@@ -8,7 +10,7 @@ function Header() {
     {/* header->mudar para o verde esmeralda */}
     <img id="logoHome" src={logo} alt="Logo" className="logo" />
     <button className="btnCadastro">Cadastre-se</button>
-    <button className="btnLogar">Login</button>
+    <button className="btnLogar"><MdLogin /></button>
   </div>
   )
 }
--- src/components/sidemenu/sidemenu.jsx
+++ src/components/sidemenu/sidemenu.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './sidemenu.css';
-
+import { MdLogin } from "react-icons/md";
+import { HiMiniPencilSquare } from "react-icons/hi2";
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -14,11 +15,17 @@ const Sidebar = () => {
         {isOpen ? '⮜' : '⮞'}
       </div>
       <ul>
+  
         <li>O produto</li>
         <li>Quem somos?</li>
         <li>...</li>
         <li>...</li>
       </ul>
+      <div className="btnMenu">
+      <button className="btnCadastroMenu">Cadastre-se</button>
+      <button className="btnLogarMenu"><MdLogin /></button>
+      </div>
+
     </div>
   );
 };
